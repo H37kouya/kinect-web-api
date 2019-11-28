1858f097f03505a12121c94fd1b22d47ae558a94
H37kouya <h37kouya@gmail.com>
2019-11-28 19:06:54 +0900

    [delete]: api/test routes is deleted.

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('test', function () {
-    return 'test';
-});
-
 Route::post('detected', 'Api\DetectedController@detected');
 
 Route::get('get-people-count', 'Api\DetectedController@getPeopleCount');
